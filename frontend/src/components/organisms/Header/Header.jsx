6d7c650c73e7b130da2da18b5856962e0abaa7ad
Rian Munawar <rianmunawar0513@gmail.com>
2023-01-16 19:44:51 +0700

    frontend: changes navbar style

--- frontend/src/components/organisms/Header/Header.jsx
+++ frontend/src/components/organisms/Header/Header.jsx
@@ -1,27 +1,27 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link } from 'react-router-dom';
-import CtaBtn from '../../atoms/CtaBtn/CtaBtn';
-import './header.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link } from "react-router-dom";
+import CtaBtn from "../../atoms/CtaBtn/CtaBtn";
+import "./header.css";
 
 function Header() {
   return (
     <>
-      {['md'].map((expand) => (
+      {["md"].map((expand) => (
         <Navbar
           key={expand}
-          as={'header'}
+          as={"header"}
           bg="light"
           expand={expand}
           fixed="top"
           className="mb-4 py-10 navbar bg-white shadow-sm"
         >
           <Container>
-            <Navbar.Brand as={'div'}>
-              <Link to={'/'}>
+            <Navbar.Brand as={"div"}>
+              <Link to={"/"}>
                 <h1> MAKDHI </h1>
               </Link>
             </Navbar.Brand>
@@ -33,40 +33,40 @@ function Header() {
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  <Link to={'/'}>
+                  <Link to={"/"}>
                     <h1> MAKDHI </h1>
                   </Link>
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3" as={'ul'}>
-                  <Nav.Link href="#action1" className="navlink" as={'li'}>
-                    <Link to={'/'}>Beranda</Link>
+                <Nav className="justify-content-end flex-grow-1 pe-3" as={"ul"}>
+                  <Nav.Link href="#action1" className="navlink" as={"li"}>
+                    <Link to={"/"}>Beranda</Link>
                   </Nav.Link>
-                  <Nav.Link className="navlink" as={'li'}>
-                    <Link to={'/profil'}>Profil</Link>
+                  <Nav.Link className="navlink" as={"li"}>
+                    <Link to={"/profil"}>Profil</Link>
                   </Nav.Link>
                   <NavDropdown
-                    as={'li'}
+                    as={"li"}
                     title="Lembaga"
-                    className="navlink fw-bold"
+                    className="fw-bold"
                     id={`offcanvasNavbarDropdown-expand-${expand}`}
                   >
                     <NavDropdown.Item className="navlink">
-                      <Link to={'/ra'}>RA Ar - Rohmah</Link>
+                      <Link to={"/ra"}>RA Ar - Rohmah</Link>
                     </NavDropdown.Item>
                     <NavDropdown.Item className="navlink">
-                      <Link to={'/MTs'}>MTs Darul Hikmah</Link>
+                      <Link to={"/mts"}>MTs Darul Hikmah</Link>
                     </NavDropdown.Item>
                     <NavDropdown.Item className="navlink">
-                      <Link to={'/MAK'}>MAK Darul Hikmah</Link>
+                      <Link to={"/mak"}>MAK Darul Hikmah</Link>
                     </NavDropdown.Item>
                   </NavDropdown>
-                  <Nav.Link className="navlink" as={'li'}>
-                    <Link to={'/blog'}>Berita</Link>
+                  <Nav.Link className="navlink" as={"li"}>
+                    <Link to={"/blog"}>Berita</Link>
                   </Nav.Link>
-                  <Nav.Link className="navlink" as={'li'}>
-                    <Link to={'/kontak'}>Kontak</Link>
+                  <Nav.Link className="navlink" as={"li"}>
+                    <Link to={"/kontak"}>Kontak</Link>
                   </Nav.Link>
                   <CtaBtn
                     className="header__btn ml-20 d-none d-sm-block"
