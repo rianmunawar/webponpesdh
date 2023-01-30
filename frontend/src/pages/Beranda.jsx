49fa06d1d18d94b3885ae4dd38a0e663f5f3592f
Rian Munawar <rianmunawar0513@gmail.com>
2023-01-30 08:26:53 +0700

    test

--- frontend/src/pages/Beranda.jsx
+++ frontend/src/pages/Beranda.jsx
@@ -6,17 +6,20 @@ import Greating from "../components/organisms/Greating/Greating";
 import History from "../components/organisms/History/History";
 import Program from "../components/organisms/Program/Program";
 import Slider from "../components/organisms/Slider";
+import Layouts from "../Layouts";
 
 const Beranda = () => {
   return (
     <>
-      <Slider />
-      <Program />
-      <Greating />
-      <History />
-      <Berita />
-      <Gallery />
-      <CtaBanner />
+      <Layouts>
+        <Slider />
+        <Program />
+        <Greating />
+        <History />
+        <Berita />
+        <Gallery />
+        <CtaBanner />
+      </Layouts>
     </>
   );
 };
